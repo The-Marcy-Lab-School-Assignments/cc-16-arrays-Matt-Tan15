c4a3fe985efa12c0907b0b7f3954d3f52c599d78
Matthew Tan <tanmatt804@gmail.com>
2021-11-16 15:36:01 +0000

    finished

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-//write your code here
+const months = ['Jan', 'February', 'March', 'April', 'May', 'June'];
+
+const replaceWithYerr = (arr, start, end) => {
+    let newArr = [];
+    for (let i = 0; i <= arr.length - 1; i++) {
+        newArr.push(arr[i]);
+      if (start === undefined && end === undefined) {
+            newArr.fill('yerr');
+        } else if (end === undefined) {
+            newArr.fill('yerr', start, arr.length);
+        } else {
+            newArr.fill('yerr', start, end + 1);
+        }
+    } 
+    
+    return newArr;
+};
+
+console.log(replaceWithYerr(months, 1, 3)); //['Jan', 'yerr', 'yerr, 'yerr', 'May', 'June']
+console.log(replaceWithYerr(months, 1)); //['Jan', 'yerr', 'yerr, 'yerr', 'yerr', 'yerr']
+console.log(replaceWithYerr(months)); //['yerr', 'yerr', 'yerr, 'yerr', 'yerr', 'yerr']
